0e6f5b9ab9d4127dcee1e27faaaaf33b7f8990ef
yuewang <ywang@arc-investments.com>
2017-01-12 18:30:25 -0500

    update triangle with diff approach

--- DP/triangle.cpp
+++ DP/triangle.cpp
@@ -29,6 +29,53 @@ public:
 
 };
 
+class Solution_divide_conquer {
+public:
+    int dfs(int x, int y, int &n, vector<vector<int>> &triangle) {
+        if (x == n) {
+            return 0;
+        }
+        return min(dfs(x + 1, y, n, triangle), dfs(x + 1, y + 1, n, triangle)) + triangle[x][y];
+    }
+
+    int minimumTotal(vector<vector<int>> &triangle) {
+        // divide and conquer version of triangle
+        int n = triangle.size();
+        int best = dfs(0, 0, n, triangle);
+        return best;
+    }
+};
+
+// remark: instead of using hashmap/std::map - its painful to use it to deal with pair values as key
+//         using vector of vector directly
+class Solution_divide_conquer_memo {
+public:
+
+    //divide and conquer with memorization
+    int dfs(int x, int y, int &n, vector<vector<int>> &triangle, vector<vector<int>> &res) {
+        if (x == n) {
+            return 0;
+        }
+
+        if (res[x][y] != -1) {
+            return res[x][y];
+        }
+        else {
+            res[x][y] = min(dfs(x + 1, y, n, triangle, res), dfs(x + 1, y + 1, n, triangle, res)) + triangle[x][y];
+            return res[x][y];
+        }
+
+    }
+
+    int minimumTotal(vector<vector<int>> &triangle) {
+        int n = triangle.size();
+        int m = triangle[n - 1].size();
+        vector<vector<int>> res(n, std::vector<int>(m, -1));
+        int best = dfs(0, 0, n, triangle, res);
+        return best;
+    }
+};
+
 
 
 //int main() {
